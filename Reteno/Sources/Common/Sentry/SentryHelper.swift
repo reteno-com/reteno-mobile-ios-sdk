82c5b2630c409fdbb345984f02d63ec5a613b593
Anna Sahaidak <anna.sahaidak@reteno.com>
2023-05-12 09:22:32 +0300

    Bump Sentry version to 8.7.0

--- Reteno/Sources/Common/Sentry/SentryHelper.swift
+++ Reteno/Sources/Common/Sentry/SentryHelper.swift
@@ -7,30 +7,47 @@
 
 import Sentry
 
-struct SentryHelper {
+final class SentryHelper {
     
-    private init() {}
+    static let shared = SentryHelper()
+    
+    private let hub: SentryHub
+    
+    private init() {
+        let options = Options()
+        options.dsn = "https://699db07fec57455e964892ba10da106f@sentry.reteno.com/4504043420450816"
+        options.enableAppHangTracking = true
+        options.enableFileIOTracing = true
+        let client = SentryClient(options: options)
+        let hub = SentryHub(client: client, andScope: Scope())
+        hub.configureScope { scope in
+            scope.setTags(
+                [
+                    "reteno.sdk_version": "\(Reteno.version)",
+                    "reteno.device_id": DeviceIdHelper.deviceId() ?? "",
+                    "reteno.bundle_id": BundleIdHelper.getMainAppBundleId()
+                ]
+            )
+        }
+        self.hub = hub
+    }
     
     static func capture(error: Error) {
-        let hub = prepareHub()
-        hub.capture(error: error)
+        shared.hub.capture(error: error)
     }
     
     static func capture(exeption: NSException) {
-        let hub = prepareHub()
-        hub.capture(exception: exeption)
+        shared.hub.capture(exception: exeption)
     }
     
     static func captureLog(title: String, count: Int) {
-        let hub = prepareHub()
         let event = Sentry.Event(level: SentryLevel.info)
         event.message = SentryMessage(formatted: "Removed \(title): - \(count)")
         event.fingerprint = [BundleIdHelper.getMainAppBundleId(), title]
-        hub.capture(event: event)
+        shared.hub.capture(event: event)
     }
     
     static func captureItems(_ items: [Groupable], title: String, tagTitle: String) {
-        let hub = prepareHub()
         let groupedItems = items.reduce(into: [String: [Groupable]]()) { result, item in
             if result[item.key].isNone {
                 result[item.key] = []
@@ -42,36 +59,16 @@ struct SentryHelper {
             event.message = SentryMessage(formatted: "\(title)(\($0.key)) - \($0.value.count)")
             event.fingerprint = [BundleIdHelper.getMainAppBundleId(), title, $0.key]
             event.tags = [tagTitle: $0.key]
-            hub.capture(event: event)
+            shared.hub.capture(event: event)
         }
     }
     
     static func captureWarningEvent(message: String, tags: [String: String]? = nil) {
-        let hub = prepareHub()
         let event = Sentry.Event(level: SentryLevel.warning)
         event.message = SentryMessage(formatted: message)
         event.fingerprint = [BundleIdHelper.getMainAppBundleId(), message]
         event.tags = tags
-        hub.capture(event: event)
-    }
-    
-    static private func prepareHub() -> SentryHub {
-        let options = Options()
-        options.dsn = "https://699db07fec57455e964892ba10da106f@sentry.reteno.com/4504043420450816"
-        options.enableAppHangTracking = true
-        options.enableFileIOTracing = true
-        
-        let client = SentryClient(options: Options())
-        let hub = SentryHub(client: client, andScope: Scope())
-        hub.scope.setTags(
-            [
-                "reteno.sdk_version": "\(Reteno.version)",
-                "reteno.device_id": DeviceIdHelper.deviceId() ?? "",
-                "reteno.bundle_id": BundleIdHelper.getMainAppBundleId()
-            ]
-        )
-        
-        return hub
+        shared.hub.capture(event: event)
     }
     
 }
